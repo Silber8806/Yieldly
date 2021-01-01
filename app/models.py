7210fe303c0dca4badf878844f70a232b82bdfb0
Chris Kottmyer <Silber8806@gmail.com>
2020-12-31 23:16:11 -0500

    table completed

--- app/models.py
+++ app/models.py
@@ -3,12 +3,24 @@ from app import db
 class Planting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     seed_id = db.Column(db.Integer, db.ForeignKey('seed.id'))
-
+    seed_grams = db.Column(db.Integer)
+    start_date = db.Column(db.Date)
+    blackout_date = db.Column(db.Date)
+    harvest_date = db.Column(db.Date)
+    harvest_grams = db.Column(db.Integer)
+    tray_length = db.Column(db.Integer)
+    tray_width = db.Column(db.Integer)
+    medium = db.Column(db.Text)
+    
     def __repr__(self):
         return '<User {}>'.format(self.id)  
 
 class Seed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    vendor = db.Column(db.Text)
+    seed_type = db.Column(db.Text)
+    batch = db.Column(db.Integer)
+    price = db.Column(db.Numeric) 
     seeds = db.relationship('Planting', backref='seed', lazy='dynamic')
 
     def __repr__(self):
